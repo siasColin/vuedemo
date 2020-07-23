53a0fa19d6f69fa1429395d2a0e0e2c693620b37
siasColin <1540247870@qq.com>
2020-07-23 18:32:39 +0800

    优化导航菜单

--- src/components/Main.vue
+++ src/components/Main.vue
@@ -1,72 +1,57 @@
 <template>
-  <el-container style="height: 100%; border: 1px solid #eee;">
-    <el-aside width="200px" style="background-color: rgb(238, 241, 246);">
-      <el-menu :default-openeds="['1', '3']">
-        <el-submenu index="1">
-          <template slot="title"
-            ><i class="el-icon-message"></i>导航一
-          </template>
-          <el-menu-item-group>
-            <template slot="title">分组一</template>
-            <el-menu-item index="1-1">选项1</el-menu-item>
-            <el-menu-item index="1-2">选项2</el-menu-item>
-          </el-menu-item-group>
-          <el-menu-item-group title="分组2">
-            <el-menu-item index="1-3">选项3</el-menu-item>
-          </el-menu-item-group>
-          <el-submenu index="1-4">
-            <template slot="title">选项4</template>
-            <el-menu-item index="1-4-1">选项4-1</el-menu-item>
-          </el-submenu>
-        </el-submenu>
-        <el-submenu index="2">
-          <template slot="title"><i class="el-icon-menu"></i>导航二</template>
-          <el-menu-item-group>
-            <template slot="title">分组一</template>
-            <el-menu-item index="2-1">选项1</el-menu-item>
-            <el-menu-item index="2-2">选项2</el-menu-item>
-          </el-menu-item-group>
-          <el-menu-item-group title="分组2">
-            <el-menu-item index="2-3">选项3</el-menu-item>
-          </el-menu-item-group>
-          <el-submenu index="2-4">
-            <template slot="title">选项4</template>
-            <el-menu-item index="2-4-1">选项4-1</el-menu-item>
-          </el-submenu>
-        </el-submenu>
-        <el-submenu index="3">
-          <template slot="title"
-            ><i class="el-icon-setting"></i>导航三
-          </template>
-          <el-menu-item-group>
-            <template slot="title">分组一</template>
-            <el-menu-item index="3-1">选项1</el-menu-item>
-            <el-menu-item index="3-2">选项2</el-menu-item>
-          </el-menu-item-group>
-          <el-menu-item-group title="分组2">
-            <el-menu-item index="3-3">选项3</el-menu-item>
-          </el-menu-item-group>
-          <el-submenu index="3-4">
-            <template slot="title">选项4</template>
-            <el-menu-item index="3-4-1">选项4-1</el-menu-item>
-          </el-submenu>
-        </el-submenu>
-      </el-menu>
-    </el-aside>
-
-    <el-container>
-      <el-header style="text-align: right; font-size: 12px;">
-        <el-dropdown>
+  <el-container style="height: 100%;">
+    <el-header class="el-header">
+      <div class="logodiv">
+        <img src="../assets/logo.png" alt="头像" />
+        <span style="font-size: 20px;">后台管理系统</span>
+      </div>
+      <div>
+        <el-dropdown @command="handleLogin">
           <i class="el-icon-setting" style="margin-right: 15px;"></i>
-          <el-dropdown-menu slot="dropdown">
-            <el-dropdown-item>查看</el-dropdown-item>
-            <el-dropdown-item>新增</el-dropdown-item>
-            <el-dropdown-item>删除</el-dropdown-item>
+          <el-dropdown-menu>
+            <el-dropdown-item command="userInfo">查看</el-dropdown-item>
+            <el-dropdown-item command="logout">退出</el-dropdown-item>
           </el-dropdown-menu>
         </el-dropdown>
-        <span>王小虎</span>
-      </el-header>
-
+        <span>colin</span>
+      </div>
+    </el-header>
+    <el-container>
+      <el-aside
+        :width="isCollapse ? '64px' : '200px'"
+        style="background-color: rgb(238, 241, 246);"
+      >
+        <div class="toggle-button" @click="toggleCollapse">|||</div>
+        <el-menu
+          :default-openeds="['1', '3']"
+          unique-opened
+          :collapse="isCollapse"
+          :collapse-transition="false"
+        >
+          <el-submenu index="1">
+            <template slot="title"
+              ><i class="el-icon-message"></i>
+              <span>一级导航1</span>
+            </template>
+            <el-menu-item index="1-1">二级导航1.1</el-menu-item>
+            <el-menu-item index="1-2">二级导航1.2</el-menu-item>
+          </el-submenu>
+          <el-submenu index="2">
+            <template slot="title"
+              ><i class="el-icon-message"></i>
+              <span>一级导航2</span>
+            </template>
+            <el-menu-item index="2-1">二级导航2.1</el-menu-item>
+            <el-submenu index="2-2">
+              <template slot="title"
+                ><i class="el-icon-message"></i>二级导航2.2
+              </template>
+              <el-menu-item index="2-2-1">三级导航2.2.1</el-menu-item>
+              <el-menu-item index="2-2-2">三级导航2.2.2</el-menu-item>
+            </el-submenu>
+          </el-submenu>
+        </el-menu>
+      </el-aside>
       <el-main>
         <el-table :data="tableData">
           <el-table-column prop="date" label="日期" width="140">
@@ -81,16 +66,29 @@
 </template>
 
 <script>
+import Storage from '../assets/js/storage'
 export default {
-  name: 'main',
   data() {
     const item = {
-      date: '2016-05-02',
-      name: '王小虎',
-      address: '上海市普陀区金沙江路 1518 弄'
+      date: '2020-07-23',
+      name: 'colin',
+      address: '北京市海淀区大钟寺'
     }
     return {
-      tableData: Array(20).fill(item)
+      tableData: Array(10).fill(item),
+      isCollapse: false
+    }
+  },
+  methods: {
+    handleLogin(command) {
+      if (command === 'logout') {
+        Storage.localRemove('Authorization')
+        Storage.localRemove('Refresh_token')
+        this.$router.push('/login')
+      }
+    },
+    toggleCollapse() {
+      this.isCollapse = !this.isCollapse
     }
   }
 }
@@ -101,9 +99,31 @@ export default {
   background-color: #b3c0d1;
   color: #333;
   line-height: 60px;
+  display: flex;
+  justify-content: space-between;
+  padding-left: 0;
 }
-
+.logodiv {
+  display: flex;
+  align-items: center;
+  img {
+    height: 90%;
+    border-radius: 50%;
+    background-color: #eeeeee;
+  }
+  span {
+    margin-left: 10px;
+  }
+}
 .el-aside {
   color: #333;
+}
+.toggle-button {
+  background-color: #eef1f6;
+  font-size: 10px;
+  line-height: 24px;
+  text-align: center;
+  letter-spacing: 0.2em;
+  cursor: pointer;
 }
 </style>
